ad4263eb983d7038d49aeb5d73e93e44b9d40da6
unicorn-84 <unicorn-84@ya.ru>
2022-02-09 23:40:43 +0300

    feat(getrelquarters): add getRelQuarters module
    Add getRelQuarters.ts, add getRelQuarters.test.ts, add docs.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as getRelYears } from './getRelYears.js';
+export { default as getRelQuarters } from './getRelQuarters.js';
 export { default as getRelDays } from './getRelDays.js';

29d3432f670374fe3ccf440903609a8232952e8e
unicorn-84 <unicorn-84@ya.ru>
2022-02-12 00:52:10 +0300

    feat(getrelhours): add the getRelHours module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as getRelYears } from './getRelYears.js';
-export { default as getRelMonths } from './getRelMonths.js';
 export { default as getRelQuarters } from './getRelQuarters.js';
+export { default as getRelMonths } from './getRelMonths.js';
 export { default as getRelDays } from './getRelDays.js';
+export { default as getRelHours } from './getRelHours.js';

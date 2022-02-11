5be425bd7ac8f290a7caa5f64ebd714eb92394dc
unicorn-84 <unicorn-84@ya.ru>
2022-02-11 17:03:19 +0300

    feat(getrelmonths): add the getRelMonths module
    add getRelMonths.ts, add getRelMonths.test.ts, add docs to README.md, change index.ts

---
+++ src/getRelMonths.ts
@@ -0,0 +1,42 @@
+/**
+ * Formats the date according to the locales, formatting options and the 'months' unit in human-friendly words relative to the current date.
+ *
+ *
+ * @param {Date} date The date to format.
+ *
+ * @param {(string|string[])} [locales] A string with a [BCP 47 language tag](https://datatracker.ietf.org/doc/html/rfc4647#section-3.4), or an array of such strings.
+ *
+ * @param {Object} [options] An [object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat#Parameters) with some or all of options of `Intl.RelativeTimeFormatOptions`.
+ *
+ * @throws `TypeError` if `date` was given something other than the Date object.
+ *
+ * @returns {string} Internationalized relative time message as string.
+ *
+ * @example
+ *
+ * getRelMonths(new Date(), 'en', { numeric: 'auto' })
+ * //=> this month
+ */
+
+const getRelMonths = (
+  date: Date,
+  locales?:
+    | Intl.UnicodeBCP47LocaleIdentifier
+    | Intl.UnicodeBCP47LocaleIdentifier[],
+  options?: Intl.RelativeTimeFormatOptions
+): string => {
+  if (!(date instanceof Date)) {
+    throw new TypeError(`[rel-time-format]: '${date}' is not the Date object`);
+  }
+
+  const formatter = new Intl.RelativeTimeFormat(locales, options);
+  console.log(new Intl.RelativeTimeFormat(undefined, {}));
+
+  const years = date.getFullYear() - new Date().getFullYear();
+
+  const diff = date.getMonth() - new Date().getMonth() + years * 12;
+
+  return formatter.format(diff, 'months');
+};
+
+export default getRelMonths;

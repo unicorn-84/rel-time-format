02b9ccc9889e483205f855b7a33c5243a55b94af
unicorn-84 <unicorn-84@ya.ru>
2022-02-11 22:06:43 +0300

    test(getrelmonths): change tests

--- src/tests/getRelMonths.test.ts
+++ src/tests/getRelMonths.test.ts
@@ -2,7 +2,7 @@ import getRelMonths from '../getRelMonths';
 
 beforeEach(() => {
   jest.useFakeTimers();
-  jest.setSystemTime(new Date('2022-3-1'));
+  jest.setSystemTime(new Date('2022-01-01'));
 });
 
 afterEach(() => {
@@ -20,20 +20,20 @@ describe.only('getRelMonths', () => {
 
   it('should return a relative time string', () => {
     expect(getRelMonths(new Date())).toBe('in 0 months');
-    expect(getRelMonths(new Date('2022-4-1'))).toBe('in 1 month');
-    expect(getRelMonths(new Date('2022-2-28'))).toBe('1 month ago');
-    expect(getRelMonths(new Date('1984-10-11'))).toBe('449 months ago');
-    expect(getRelMonths(new Date('2084-10-11'))).toBe('in 751 months');
+    expect(getRelMonths(new Date('2022-02-01'))).toBe('in 1 month');
+    expect(getRelMonths(new Date('2021-12-01'))).toBe('1 month ago');
+    expect(getRelMonths(new Date('1984-10-11'))).toBe('447 months ago');
+    expect(getRelMonths(new Date('2084-10-11'))).toBe('in 753 months');
   });
 
   it('should return a relative time string according to the locales', () => {
     expect(getRelMonths(new Date(), 'ru')).toBe('через 0 месяцев');
-    expect(getRelMonths(new Date('2022-4-1'), 'ru')).toBe('через 1 месяц');
-    expect(getRelMonths(new Date('2022-2-28'), 'ru')).toBe('1 месяц назад');
+    expect(getRelMonths(new Date('2022-02-01'), 'ru')).toBe('через 1 месяц');
+    expect(getRelMonths(new Date('2021-12-01'), 'ru')).toBe('1 месяц назад');
     expect(getRelMonths(new Date('1984-10-11'), 'ru')).toBe(
-      '449 месяцев назад'
+      '447 месяцев назад'
     );
-    expect(getRelMonths(new Date('2084-10-11'), 'ru')).toBe('через 751 месяц');
+    expect(getRelMonths(new Date('2084-10-11'), 'ru')).toBe('через 753 месяца');
   });
 
   it('should return a relative time string according to the formatting options', () => {
@@ -41,34 +41,34 @@ describe.only('getRelMonths', () => {
       'this month'
     );
     expect(
-      getRelMonths(new Date('2022-4-1'), undefined, { numeric: 'auto' })
+      getRelMonths(new Date('2022-02-01'), undefined, { numeric: 'auto' })
     ).toBe('next month');
     expect(
-      getRelMonths(new Date('2022-2-28'), undefined, { numeric: 'auto' })
+      getRelMonths(new Date('2021-12-01'), undefined, { numeric: 'auto' })
     ).toBe('last month');
     expect(
       getRelMonths(new Date('1984-10-11'), undefined, { numeric: 'auto' })
-    ).toBe('449 months ago');
+    ).toBe('447 months ago');
     expect(
       getRelMonths(new Date('2084-10-11'), undefined, { numeric: 'auto' })
-    ).toBe('in 751 months');
+    ).toBe('in 753 months');
   });
 
   it('should return a relative time string according to the locales and the formatting options', () => {
     expect(getRelMonths(new Date(), 'ru', { numeric: 'auto' })).toBe(
       'в этом месяце'
     );
-    expect(getRelMonths(new Date('2022-4-1'), 'ru', { numeric: 'auto' })).toBe(
-      'в следующем месяце'
-    );
-    expect(getRelMonths(new Date('2022-2-28'), 'ru', { numeric: 'auto' })).toBe(
-      'в прошлом месяце'
-    );
+    expect(
+      getRelMonths(new Date('2022-02-01'), 'ru', { numeric: 'auto' })
+    ).toBe('в следующем месяце');
+    expect(
+      getRelMonths(new Date('2021-12-01'), 'ru', { numeric: 'auto' })
+    ).toBe('в прошлом месяце');
     expect(
       getRelMonths(new Date('1984-10-11'), 'ru', { numeric: 'auto' })
-    ).toBe('449 месяцев назад');
+    ).toBe('447 месяцев назад');
     expect(
       getRelMonths(new Date('2084-10-11'), 'ru', { numeric: 'auto' })
-    ).toBe('через 751 месяц');
+    ).toBe('через 753 месяца');
   });
 });

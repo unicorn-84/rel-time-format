d43abea33ea5a3daf97e0a4899636527cb183c22
unicorn-84 <unicorn-84@ya.ru>
2022-02-15 22:26:31 +0300

    refactor(getrelweeks): change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as getRelYears } from './getRelYears.js';
 export { default as getRelQuarters } from './getRelQuarters.js';
 export { default as getRelMonths } from './getRelMonths.js';
+export { default as getRelWeeks } from './getRelWeeks.js';
 export { default as getRelDays } from './getRelDays.js';
 export { default as getRelHours } from './getRelHours.js';
 export { default as getRelMinutes } from './getRelMinutes.js';

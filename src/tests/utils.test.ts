b8524ebccbfa32a34c42d05e10ed9aa5c518af6c
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 17:36:59 +0300

    refactor(utils): add getDiffInCalendarMonths

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { getDiffInCalendarYears } from '../utils';
+import { getDiffInCalendarYears, getDiffInCalendarMonths } from '../utils';
 
 beforeEach(() => {
   jest.useFakeTimers();
@@ -32,5 +32,35 @@ describe('getDiffInCalendarYears', () => {
     expect(
       getDiffInCalendarYears(new Date('1984-10-11'), new Date(), false)
     ).toBe(-38);
+  });
+});
+
+describe('getDiffInCalendarMonths', () => {
+  it('should return the positive number of calendar months', () => {
+    expect(getDiffInCalendarMonths(new Date(), new Date())).toBe(0);
+    expect(getDiffInCalendarMonths(new Date('2022-02-01'), new Date())).toBe(1);
+    expect(
+      getDiffInCalendarMonths(new Date('2022-01-01'), new Date('2021-01-01'))
+    ).toBe(12);
+  });
+
+  it('should return the negative number of calendar months', () => {
+    expect(
+      getDiffInCalendarMonths(new Date('2021-12-31'), new Date(), false)
+    ).toBe(-1);
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2023-01-01'),
+        new Date('2025-01-01'),
+        false
+      )
+    ).toBe(-24);
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2020-02-29'),
+        new Date('2020-03-01'),
+        false
+      )
+    ).toBe(-1);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -19,3 +19,27 @@ export const getDiffInCalendarYears = (
 
   return abs ? Math.abs(years) : years;
 };
+
+/**
+ * Get the number of calendar months between the given dates.
+ *
+ * @param {Date} targetDate The target date.
+ *
+ * @param {Date} baseDate The base date.
+ *
+ * @param {boolean} [abs=true] If `true` function returns the absolute value of a number.
+ *
+ * @returns The number of calendar months.
+ */
+
+export const getDiffInCalendarMonths = (
+  targetDate: Date,
+  baseDate: Date,
+  abs = true
+) => {
+  const years = getDiffInCalendarYears(targetDate, baseDate, false);
+
+  const months = targetDate.getMonth() - baseDate.getMonth() + years * 12;
+
+  return abs ? Math.abs(months) : months;
+};

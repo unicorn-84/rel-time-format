5b46623cd2e595cdb9f20ea424536d89a0f9a695
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 19:21:14 +0300

    docs(utils): change docs for getDiffInCalendarDays

--- src/utils.ts
+++ src/utils.ts
@@ -131,8 +131,8 @@ export const getDiffInCalendarWeeks = (
  *
  * @example
  *
- * getDiffInCalendarDays(new Date('2022-01-01T00:00:00'), new Date('2021-01-01T23:59:59'))
- * //=> 365
+ * getDiffInCalendarDays(new Date('2022-01-01T00:00:00'), new Date('2021-12-31T23:59:59'))
+ * //=> 1
  */
 
 export const getDiffInCalendarDays = (

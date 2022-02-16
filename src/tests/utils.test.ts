9da3406a5ed736fc59512ec8c490edbe65e1981a
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 22:49:43 +0300

    test(utils): change getDiffInCalendarDays tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -350,16 +350,26 @@ describe('getDiffInCalendarDays', () => {
     expect(getDiffInCalendarDays(new Date(), new Date())).toBe(
       differenceInCalendarDays(new Date(), new Date())
     );
+    expect(getDiffInCalendarDays(new Date(), new Date())).toBe(0);
+
     expect(
       getDiffInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
     ).toBe(
       differenceInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
     );
     expect(
-      getDiffInCalendarDays(new Date(), new Date('2021-01-01T23:59:59'))
+      getDiffInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
+    ).toBe(1);
+
+    expect(
+      getDiffInCalendarDays(new Date('2022-01-01T23:59:59'), new Date())
     ).toBe(
-      differenceInCalendarDays(new Date(), new Date('2021-01-01T23:59:59'))
+      differenceInCalendarDays(new Date('2022-01-01T23:59:59'), new Date())
     );
+    expect(
+      getDiffInCalendarDays(new Date('2022-01-01T23:59:59'), new Date())
+    ).toBe(0);
+
     expect(
       getDiffInCalendarDays(
         new Date('2022-01-01T00:00:00'),
@@ -371,6 +381,12 @@ describe('getDiffInCalendarDays', () => {
         new Date('2021-12-31T23:59:59')
       )
     );
+    expect(
+      getDiffInCalendarDays(
+        new Date('2022-01-01T00:00:00'),
+        new Date('2021-12-31T23:59:59')
+      )
+    ).toBe(1);
   });
 
   it('should return the negative number of calendar days', () => {
@@ -380,10 +396,18 @@ describe('getDiffInCalendarDays', () => {
       differenceInCalendarDays(new Date('2021-12-31T23:59:59'), new Date())
     );
     expect(
-      getDiffInCalendarDays(new Date('2021-01-01T23:59:59'), new Date(), false)
+      getDiffInCalendarDays(new Date('2021-12-31T23:59:59'), new Date(), false)
+    ).toBe(-1);
+
+    expect(
+      getDiffInCalendarDays(new Date(), new Date('2022-02-01T23:59:59'), false)
     ).toBe(
-      differenceInCalendarDays(new Date('2021-01-01T23:59:59'), new Date())
+      differenceInCalendarDays(new Date(), new Date('2022-02-01T23:59:59'))
     );
+    expect(
+      getDiffInCalendarDays(new Date(), new Date('2022-02-01T23:59:59'), false)
+    ).toBe(-31);
+
     expect(
       getDiffInCalendarDays(
         new Date('2021-01-01T00:00:00'),
@@ -396,5 +420,12 @@ describe('getDiffInCalendarDays', () => {
         new Date('2022-01-01T23:59:59')
       )
     );
+    expect(
+      getDiffInCalendarDays(
+        new Date('2021-01-01T00:00:00'),
+        new Date('2022-01-01T23:59:59'),
+        false
+      )
+    ).toBe(-365);
   });
 });

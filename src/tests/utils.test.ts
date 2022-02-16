c02fbfa08b0a0c2b7c32f0b4ef4be4bf0053f848
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 19:11:03 +0300

    test(utils): change tests for getDiffInCalendarDays

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -136,11 +136,17 @@ describe('getDiffInCalendarDays', () => {
   it('should return the positive number of calendar days', () => {
     expect(getDiffInCalendarDays(new Date(), new Date())).toBe(0);
     expect(
-      getDiffInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'), false)
+      getDiffInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
     ).toBe(1);
     expect(
       getDiffInCalendarDays(new Date(), new Date('2021-01-01T23:59:59'))
     ).toBe(365);
+    expect(
+      getDiffInCalendarDays(
+        new Date('2022-01-01T00:00:00'),
+        new Date('2021-12-31T23:59:59')
+      )
+    ).toBe(1);
   });
 
   it('should return the negative number of calendar days', () => {
@@ -148,7 +154,14 @@ describe('getDiffInCalendarDays', () => {
       getDiffInCalendarDays(new Date('2021-12-31T23:59:59'), new Date(), false)
     ).toBe(-1);
     expect(
-      getDiffInCalendarDays(new Date('2021-01-01T23:59:59'), new Date())
-    ).toBe(365);
+      getDiffInCalendarDays(new Date('2021-01-01T23:59:59'), new Date(), false)
+    ).toBe(-365);
+    expect(
+      getDiffInCalendarDays(
+        new Date('2021-01-01T00:00:00'),
+        new Date('2022-01-01T23:59:59'),
+        false
+      )
+    ).toBe(-365);
   });
 });

23208a6276f8f663dfd5fb38581f676490aeadd5
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 22:32:47 +0300

    test(utils): change getDiffInCalendarMonths tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -154,38 +154,99 @@ describe('getDiffInCalendarMonths', () => {
     expect(getDiffInCalendarMonths(new Date(), new Date())).toBe(
       differenceInCalendarMonths(new Date(), new Date())
     );
-    expect(getDiffInCalendarMonths(new Date('2022-02-01'), new Date())).toBe(
-      differenceInCalendarMonths(new Date('2022-02-01'), new Date())
+    expect(getDiffInCalendarMonths(new Date(), new Date())).toBe(0);
+
+    expect(
+      getDiffInCalendarMonths(new Date(), new Date('2021-02-01T23:59:59'))
+    ).toBe(
+      differenceInCalendarMonths(new Date(), new Date('2021-02-01T23:59:59'))
     );
     expect(
-      getDiffInCalendarMonths(new Date('2022-01-01'), new Date('2021-01-01'))
+      getDiffInCalendarMonths(new Date(), new Date('2021-02-01T23:59:59'))
+    ).toBe(11);
+
+    expect(
+      getDiffInCalendarMonths(new Date('2022-02-01T23:59:59'), new Date())
     ).toBe(
-      differenceInCalendarMonths(new Date('2022-01-01'), new Date('2021-01-01'))
+      differenceInCalendarMonths(new Date('2022-02-01T23:59:59'), new Date())
     );
+    expect(
+      getDiffInCalendarMonths(new Date('2022-02-01T23:59:59'), new Date())
+    ).toBe(1);
+
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2022-01-01T23:59:59'),
+        new Date('2021-01-01T23:59:59')
+      )
+    ).toBe(
+      differenceInCalendarMonths(
+        new Date('2022-01-01T23:59:59'),
+        new Date('2021-01-01T23:59:59')
+      )
+    );
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2022-01-01T23:59:59'),
+        new Date('2021-01-01T23:59:59')
+      )
+    ).toBe(12);
   });
 
   it('should return the negative number of calendar months', () => {
     expect(
-      getDiffInCalendarMonths(new Date('2021-12-31'), new Date(), false)
-    ).toBe(differenceInCalendarMonths(new Date('2021-12-31'), new Date()));
-    expect(
       getDiffInCalendarMonths(
-        new Date('2023-01-01'),
-        new Date('2025-01-01'),
+        new Date('2021-12-31T23:59:59'),
+        new Date(),
         false
       )
     ).toBe(
-      differenceInCalendarMonths(new Date('2023-01-01'), new Date('2025-01-01'))
+      differenceInCalendarMonths(new Date('2021-12-31T23:59:59'), new Date())
     );
     expect(
       getDiffInCalendarMonths(
-        new Date('2020-02-29'),
-        new Date('2020-03-01'),
+        new Date('2021-12-31T23:59:59'),
+        new Date(),
+        false
+      )
+    ).toBe(-1);
+
+    expect(
+      getDiffInCalendarMonths(
+        new Date(),
+        new Date('2025-01-01T23:59:59'),
         false
       )
     ).toBe(
-      differenceInCalendarMonths(new Date('2020-02-29'), new Date('2020-03-01'))
+      differenceInCalendarMonths(new Date(), new Date('2025-01-01T23:59:59'))
     );
+    expect(
+      getDiffInCalendarMonths(
+        new Date(),
+        new Date('2025-01-01T23:59:59'),
+        false
+      )
+    ).toBe(-36);
+
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2020-02-29T23:59:59'),
+        new Date('2020-03-01T23:59:59'),
+        false
+      )
+    ).toBe(
+      differenceInCalendarMonths(
+        new Date('2020-02-29T23:59:59'),
+        new Date('2020-03-01T23:59:59')
+      )
+    );
+    expect(
+      getDiffInCalendarMonths(
+        new Date('2020-02-29T23:59:59'),
+        new Date('2020-03-01T23:59:59'),
+        false
+      )
+    ).toBe(-1);
   });
 });
 

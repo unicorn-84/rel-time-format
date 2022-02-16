ae527d733ffec938c5cfb527a6308fe049ac53c9
unicorn-84 <unicorn-84@ya.ru>
2022-02-17 00:52:01 +0300

    refactor(utils): change getDiffInCalendarHours, change getDiffInCalendarHours tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -11,6 +11,7 @@ import {
   getDiffInCalendarMonths,
   getDiffInCalendarWeeks,
   getDiffInCalendarDays,
+  getDiffInCalendarHours,
 } from '../utils';
 
 beforeEach(() => {
@@ -478,5 +479,44 @@ describe('getDiffInCalendarDays', () => {
         false
       )
     ).toBe(-365);
+  });
+});
+
+describe('getDiffInCalendarHours', () => {
+  it('should return the positive number of calendar hours', () => {
+    expect(getDiffInCalendarHours(new Date(), new Date())).toBe(0);
+
+    expect(
+      getDiffInCalendarHours(new Date(), new Date('2021-12-31T23:59:59'))
+    ).toBe(1);
+
+    expect(
+      getDiffInCalendarHours(new Date('2022-01-01T23:59:59'), new Date())
+    ).toBe(23);
+
+    expect(
+      getDiffInCalendarHours(
+        new Date('2022-01-01T23:59:59'),
+        new Date('2021-12-31T00:59:59')
+      )
+    ).toBe(47);
+  });
+
+  it('should return the negative number of calendar hours', () => {
+    expect(
+      getDiffInCalendarHours(new Date(), new Date('2022-01-01T23:59:59'), false)
+    ).toBe(-23);
+
+    expect(
+      getDiffInCalendarHours(new Date('2021-12-31T23:59:59'), new Date(), false)
+    ).toBe(-1);
+
+    expect(
+      getDiffInCalendarHours(
+        new Date('2021-01-01T23:59:59'),
+        new Date('2021-01-02T23:59:59'),
+        false
+      )
+    ).toBe(-24);
   });
 });

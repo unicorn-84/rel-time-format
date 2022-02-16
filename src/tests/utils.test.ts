32bedb5e6ef0fb46135c54f3a6e6cd22d66c5ccd
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 22:58:41 +0300

    test(utils): add date-fns for getDiffInCalendarYears tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,4 +1,5 @@
 import {
+  differenceInCalendarYears,
   differenceInCalendarQuarters,
   differenceInCalendarMonths,
   differenceInCalendarWeeks,
@@ -23,27 +24,77 @@ afterEach(() => {
 
 describe('getDiffInCalendarYears', () => {
   it('should return the positive number of calendar years', () => {
+    expect(getDiffInCalendarYears(new Date(), new Date())).toBe(
+      differenceInCalendarYears(new Date(), new Date())
+    );
     expect(getDiffInCalendarYears(new Date(), new Date())).toBe(0);
-    expect(getDiffInCalendarYears(new Date('2023-01-01'), new Date())).toBe(1);
+
     expect(
-      getDiffInCalendarYears(new Date('2021-01-01'), new Date('2023-01-01'))
+      getDiffInCalendarYears(new Date(), new Date('2020-01-01T23:59:59'))
+    ).toBe(
+      differenceInCalendarYears(new Date(), new Date('2020-01-01T23:59:59'))
+    );
+    expect(
+      getDiffInCalendarYears(new Date(), new Date('2020-01-01T23:59:59'))
     ).toBe(2);
+
+    expect(
+      getDiffInCalendarYears(
+        new Date('2024-01-01T23:59:59'),
+        new Date('2023-01-01T23:59:59')
+      )
+    ).toBe(
+      differenceInCalendarYears(
+        new Date('2024-01-01T23:59:59'),
+        new Date('2023-01-01T23:59:59')
+      )
+    );
+    expect(
+      getDiffInCalendarYears(
+        new Date('2024-01-01T23:59:59'),
+        new Date('2023-01-01T23:59:59')
+      )
+    ).toBe(1);
   });
 
   it('should return the negative number of calendar years', () => {
     expect(
-      getDiffInCalendarYears(new Date('2021-01-01'), new Date(), false)
+      getDiffInCalendarYears(new Date(), new Date('2023-01-01T23:59:59'), false)
+    ).toBe(
+      differenceInCalendarYears(new Date(), new Date('2023-01-01T23:59:59'))
+    );
+    expect(
+      getDiffInCalendarYears(new Date(), new Date('2023-01-01T23:59:59'), false)
     ).toBe(-1);
+
+    expect(
+      getDiffInCalendarYears(new Date('1984-10-11T23:59:59'), new Date(), false)
+    ).toBe(
+      differenceInCalendarYears(new Date('1984-10-11T23:59:59'), new Date())
+    );
+    expect(
+      getDiffInCalendarYears(new Date('1984-10-11T23:59:59'), new Date(), false)
+    ).toBe(-38);
+
     expect(
       getDiffInCalendarYears(
-        new Date('2023-01-01'),
-        new Date('2025-01-01'),
+        new Date('2023-01-01T23:59:59'),
+        new Date('2025-01-01T23:59:59'),
+        false
+      )
+    ).toBe(
+      differenceInCalendarYears(
+        new Date('2023-01-01T23:59:59'),
+        new Date('2025-01-01T23:59:59')
+      )
+    );
+    expect(
+      getDiffInCalendarYears(
+        new Date('2023-01-01T23:59:59'),
+        new Date('2025-01-01T23:59:59'),
         false
       )
     ).toBe(-2);
-    expect(
-      getDiffInCalendarYears(new Date('1984-10-11'), new Date(), false)
-    ).toBe(-38);
   });
 });
 

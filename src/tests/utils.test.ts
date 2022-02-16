7b4e0b12547475da7a8ba5858489ffededdf251a
unicorn-84 <unicorn-84@ya.ru>
2022-02-16 21:24:29 +0300

    test(utils): add date-fns for getDiffInCalendarDays tests

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,3 +1,4 @@
+import { differenceInCalendarDays } from 'date-fns';
 import {
   getDiffInCalendarYears,
   getDiffInCalendarQuarters,
@@ -139,34 +140,54 @@ describe('getDiffInCalendarWeeks', () => {
 
 describe('getDiffInCalendarDays', () => {
   it('should return the positive number of calendar days', () => {
-    expect(getDiffInCalendarDays(new Date(), new Date())).toBe(0);
+    expect(getDiffInCalendarDays(new Date(), new Date())).toBe(
+      differenceInCalendarDays(new Date(), new Date())
+    );
     expect(
       getDiffInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
-    ).toBe(1);
+    ).toBe(
+      differenceInCalendarDays(new Date(), new Date('2021-12-31T23:59:59'))
+    );
     expect(
       getDiffInCalendarDays(new Date(), new Date('2021-01-01T23:59:59'))
-    ).toBe(365);
+    ).toBe(
+      differenceInCalendarDays(new Date(), new Date('2021-01-01T23:59:59'))
+    );
     expect(
       getDiffInCalendarDays(
         new Date('2022-01-01T00:00:00'),
         new Date('2021-12-31T23:59:59')
       )
-    ).toBe(1);
+    ).toBe(
+      differenceInCalendarDays(
+        new Date('2022-01-01T00:00:00'),
+        new Date('2021-12-31T23:59:59')
+      )
+    );
   });
 
   it('should return the negative number of calendar days', () => {
     expect(
       getDiffInCalendarDays(new Date('2021-12-31T23:59:59'), new Date(), false)
-    ).toBe(-1);
+    ).toBe(
+      differenceInCalendarDays(new Date('2021-12-31T23:59:59'), new Date())
+    );
     expect(
       getDiffInCalendarDays(new Date('2021-01-01T23:59:59'), new Date(), false)
-    ).toBe(-365);
+    ).toBe(
+      differenceInCalendarDays(new Date('2021-01-01T23:59:59'), new Date())
+    );
     expect(
       getDiffInCalendarDays(
         new Date('2021-01-01T00:00:00'),
         new Date('2022-01-01T23:59:59'),
         false
       )
-    ).toBe(-365);
+    ).toBe(
+      differenceInCalendarDays(
+        new Date('2021-01-01T00:00:00'),
+        new Date('2022-01-01T23:59:59')
+      )
+    );
   });
 });

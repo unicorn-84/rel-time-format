4c45f44861dee374c811757ae55168b772c020d7
unicorn-84 <unicorn-84@ya.ru>
2022-02-17 19:26:27 +0300

    refactor(utils): add getDiffInCalendarSeconds

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -13,6 +13,7 @@ import {
   getDiffInCalendarDays,
   getDiffInCalendarHours,
   getDiffInCalendarMinutes,
+  getDiffInCalendarSeconds,
 } from '../utils';
 
 beforeEach(() => {
@@ -566,5 +567,52 @@ describe('getDiffInCalendarMinutes', () => {
         false
       )
     ).toBe(-1);
+  });
+});
+
+describe('getDiffInCalendarSeconds', () => {
+  it('should return the positive number of calendar seconds', () => {
+    expect(getDiffInCalendarSeconds(new Date(), new Date())).toBe(0);
+
+    expect(
+      getDiffInCalendarSeconds(new Date(), new Date('2021-12-31T23:59:59.600'))
+    ).toBe(1);
+
+    expect(
+      getDiffInCalendarSeconds(new Date('2022-01-01T00:00:59.900'), new Date())
+    ).toBe(59);
+
+    expect(
+      getDiffInCalendarSeconds(
+        new Date('2022-01-01T00:00:59.100'),
+        new Date('2021-12-31T23:59:59.200')
+      )
+    ).toBe(60);
+  });
+
+  it('should return the negative number of calendar seconds', () => {
+    expect(
+      getDiffInCalendarSeconds(
+        new Date(),
+        new Date('2022-01-01T00:00:59.200'),
+        false
+      )
+    ).toBe(-59);
+
+    expect(
+      getDiffInCalendarSeconds(
+        new Date('2021-12-31T23:59:59.100'),
+        new Date(),
+        false
+      )
+    ).toBe(-1);
+
+    expect(
+      getDiffInCalendarSeconds(
+        new Date('2021-01-01T23:59:59.100'),
+        new Date('2021-01-02T00:00:59.100'),
+        false
+      )
+    ).toBe(-60);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -223,3 +223,32 @@ export const getDiffInCalendarMinutes = (
 
   return abs ? Math.abs(minutes) : minutes;
 };
+
+/**
+ * Get the number of calendar seconds between the given dates. This means that milliseconds are removed from the dates.
+ *
+ * @param {Date} targetDate The target date.
+ *
+ * @param {Date} baseDate The base date.
+ *
+ * @param {boolean} [abs=true] If `true` function returns the absolute value of a number.
+ *
+ * @returns The number of calendar seconds.
+ *
+ * @example
+ *
+ * getDiffInCalendarSeconds(new Date('2022-01-01T00:00:59.100'), new Date('2021-12-31T23:59:59.200'))
+ * //=> 60
+ */
+
+export const getDiffInCalendarSeconds = (
+  targetDate: Date,
+  baseDate: Date,
+  abs = true
+) => {
+  const diff = targetDate.setMilliseconds(0) - baseDate.setMilliseconds(0);
+
+  const seconds = Math.trunc(diff / 1000);
+
+  return abs ? Math.abs(seconds) : seconds;
+};

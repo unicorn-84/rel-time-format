85d521596173174ef272b65bfc45ba1cd01ed9d2
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 15:07:55 +0300

    feat(lib): add getDiffInCalendarHours

--- src/getDiffInCalendarHours.ts
+++ src/getDiffInCalendarHours.ts
@@ -2,23 +2,18 @@
  * Get the number of calendar hours between the given dates. This means that minutes, seconds and milliseconds are removed from the dates.
  *
  * @param {Date} targetDate The target date.
- *
  * @param {Date} baseDate The base date.
- *
- * @param {boolean} [abs=true] If `true` function returns the absolute value of a number. Default: `true`.
- *
+ * @param {boolean} [abs=false] If `true` function returns the absolute value of a number. Default: `false`.
  * @returns The number of calendar hours.
- *
  * @example
- *
- * getDiffInCalendarHours(new Date('2022-01-01T23:59:59'), new Date('2021-12-31T00:59:59'))
- * //=> 47
+ * getDiffInCalendarHours(new Date('2021-12-31T00:59:59'), new Date('2022-01-01T23:59:59')) //=> 47
+ * getDiffInCalendarHours(new Date('2021-01-01T23:59:59'), new Date('2021-01-02T23:59:59'), true) //=> 24
  */
 
 const getDiffInCalendarHours = (
   targetDate: Date,
   baseDate: Date,
-  abs = true
+  abs = false
 ) => {
   const diff = targetDate.setMinutes(0, 0, 0) - baseDate.setMinutes(0, 0, 0);
 
--- src/tests/getDiffInCalendarHours.test.ts
+++ src/tests/getDiffInCalendarHours.test.ts
@@ -10,7 +10,7 @@ afterEach(() => {
 });
 
 describe('getDiffInCalendarHours', () => {
-  it('should return the positive number of calendar hours', () => {
+  it('should return the number of calendar hours', () => {
     expect(getDiffInCalendarHours(new Date(), new Date())).toBe(0);
 
     expect(
@@ -23,27 +23,17 @@ describe('getDiffInCalendarHours', () => {
 
     expect(
       getDiffInCalendarHours(
-        new Date('2022-01-01T23:59:59'),
-        new Date('2021-12-31T00:59:59')
+        new Date('2021-12-31T00:59:59'),
+        new Date('2022-01-01T23:59:59')
       )
-    ).toBe(47);
-  });
-
-  it('should return the negative number of calendar hours', () => {
-    expect(
-      getDiffInCalendarHours(new Date(), new Date('2022-01-01T23:59:59'), false)
-    ).toBe(-23);
-
-    expect(
-      getDiffInCalendarHours(new Date('2021-12-31T23:59:59'), new Date(), false)
-    ).toBe(-1);
+    ).toBe(-47);
 
     expect(
       getDiffInCalendarHours(
         new Date('2021-01-01T23:59:59'),
         new Date('2021-01-02T23:59:59'),
-        false
+        true
       )
-    ).toBe(-24);
+    ).toBe(24);
   });
 });

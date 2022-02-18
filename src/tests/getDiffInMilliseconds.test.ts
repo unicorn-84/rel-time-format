049a7365a31968dd68d73df8ab4e6a9ad4a6e243
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 14:54:57 +0300

    test(lib): change getDiffInMilliseconds tests

--- src/tests/getDiffInMilliseconds.test.ts
+++ src/tests/getDiffInMilliseconds.test.ts
@@ -23,17 +23,17 @@ describe('getDiffInMilliseconds', () => {
 
     expect(
       getDiffInMilliseconds(
+        new Date('2021-01-01T23:59:59.100'),
+        new Date('2021-01-02T00:00:00.100')
+      )
+    ).toBe(-1000);
+
+    expect(
+      getDiffInMilliseconds(
         new Date('2021-12-31T23:59:59.200'),
         new Date('2022-01-01T00:00:00.100'),
         true
       )
     ).toBe(900);
-
-    expect(
-      getDiffInMilliseconds(
-        new Date('2021-01-01T23:59:59.100'),
-        new Date('2021-01-02T00:00:00.100')
-      )
-    ).toBe(-1000);
   });
 });

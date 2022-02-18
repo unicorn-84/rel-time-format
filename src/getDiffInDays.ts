17ff12184e24d5856b5e8fc9722a7fbae4f745f6
unicorn-84 <unicorn-84@ya.ru>
2022-02-19 00:47:51 +0300

    docs(lib): change docs for getDiffInDays

--- src/getDiffInDays.ts
+++ src/getDiffInDays.ts
@@ -8,8 +8,8 @@ import getDiffInMilliseconds from './getDiffInMilliseconds.js';
  * @param {boolean} [abs=false] If `true` function returns the absolute value of a number. Default: `false`.
  * @returns The number of full days.
  * @example
- * getDiffInHours(new Date('2021-12-31T00:59:59'), new Date('2022-01-01T23:58:59')) //=> -46
- * getDiffInHours(new Date('2021-01-01T23:59:59'), new Date('2021-01-02T23:58:59'), true) //=> 23
+ * getDiffInDays(new Date('2021-12-30T23:59:59'), new Date('2022-01-01T00:00:00')) //=> -1
+ * getDiffInDays(new Date('2021-01-01T00:00:00'), new Date('2022-01-01T23:59:59'), true) //=> 365
  */
 
 const getDiffInDays = (targetDate: Date, baseDate: Date, abs = false) => {

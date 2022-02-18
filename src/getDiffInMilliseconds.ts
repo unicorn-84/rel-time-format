5935595a0a743e04ce77af0f048d915e838da30d
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 14:42:14 +0300

    feat(lib): add getDiffInMilliseconds

--- src/getDiffInMilliseconds.ts
+++ src/getDiffInMilliseconds.ts
@@ -2,23 +2,18 @@
  * Get the number of milliseconds between the given dates.
  *
  * @param {Date} targetDate The target date.
- *
  * @param {Date} baseDate The base date.
- *
- * @param {boolean} [abs=true] If `true` function returns the absolute value of a number. Default: `true`.
- *
+ * @param {boolean} [abs=false] If `true` function returns the absolute value of a number. Default: `false`.
  * @returns The number of milliseconds.
- *
  * @example
- *
- * getDiffInMilliseconds(new Date('2022-01-01T00:00:00.100'), new Date('2021-12-31T23:59:59.200'))
- * //=> 900
+ * getDiffInMilliseconds(new Date('2021-01-01T23:59:59.100'), new Date('2021-01-02T00:00:00.100')) //=> -1000
+ * getDiffInMilliseconds(new Date('2021-12-31T23:59:59.200'), new Date('2022-01-01T00:00:00.100'), true) //=> 900
  */
 
 const getDiffInMilliseconds = (
   targetDate: Date,
   baseDate: Date,
-  abs = true
+  abs = false
 ) => {
   const milliseconds = targetDate.getTime() - baseDate.getTime();
 
--- src/tests/getDiffInMilliseconds.test.ts
+++ src/tests/getDiffInMilliseconds.test.ts
@@ -10,7 +10,7 @@ afterEach(() => {
 });
 
 describe('getDiffInMilliseconds', () => {
-  it('should return the positive number of milliseconds', () => {
+  it('should return the number of milliseconds', () => {
     expect(getDiffInMilliseconds(new Date(), new Date())).toBe(0);
 
     expect(
@@ -23,34 +23,16 @@ describe('getDiffInMilliseconds', () => {
 
     expect(
       getDiffInMilliseconds(
+        new Date('2021-12-31T23:59:59.200'),
         new Date('2022-01-01T00:00:00.100'),
-        new Date('2021-12-31T23:59:59.200')
+        true
       )
     ).toBe(900);
-  });
-
-  it('should return the negative number of milliseconds', () => {
-    expect(
-      getDiffInMilliseconds(
-        new Date(),
-        new Date('2022-01-01T00:00:00.200'),
-        false
-      )
-    ).toBe(-200);
-
-    expect(
-      getDiffInMilliseconds(
-        new Date('2021-12-31T23:59:59.100'),
-        new Date(),
-        false
-      )
-    ).toBe(-900);
 
     expect(
       getDiffInMilliseconds(
         new Date('2021-01-01T23:59:59.100'),
-        new Date('2021-01-02T00:00:00.100'),
-        false
+        new Date('2021-01-02T00:00:00.100')
       )
     ).toBe(-1000);
   });

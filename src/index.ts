9d209ed72f8742396e7b4b0971a47a6186f99b4e
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 16:14:24 +0300

    feat(lib): add getDiffInCalendarYears

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export { default as getRelHours } from './getRelHours.js';
 export { default as getRelMinutes } from './getRelMinutes.js';
 export { default as getRelSeconds } from './getRelSeconds.js';
 
+export { default as getDiffInCalendarYears } from './getDiffInCalendarYears.js';
 export { default as getDiffInCalendarQuarters } from './getDiffInCalendarQuarters.js';
 export { default as getDiffInCalendarMonths } from './getDiffInCalendarMonths.js';
 export { default as getDiffInCalendarWeeks } from './getDiffInCalendarWeeks.js';

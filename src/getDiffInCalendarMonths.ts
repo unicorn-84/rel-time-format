4e35e301173a12a9b2d0722713837f420b22d0d8
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 15:52:51 +0300

    feat(lib): add getDiffInCalendarMonths

--- src/getDiffInCalendarMonths.ts
+++ src/getDiffInCalendarMonths.ts
@@ -2,17 +2,12 @@
  * Get the number of calendar months between the given dates.
  *
  * @param {Date} targetDate The target date.
- *
  * @param {Date} baseDate The base date.
- *
- * @param {boolean} [abs=true] If `true` function returns the absolute value of a number. Default: `true`.
- *
+ * @param {boolean} [abs=false] If `true` function returns the absolute value of a number. Default: `false`.
  * @returns The number of calendar months.
- *
  * @example
- *
- * getDiffInCalendarMonths(new Date('2022-01-01'), new Date('2021-01-01'))
- * //=> 12
+ * getDiffInCalendarMonths(new Date('2021-01-01T23:59:59'), new Date('2022-01-01T23:59:59')) //=> -12
+ * getDiffInCalendarMonths(new Date('2020-02-29T23:59:59'), new Date('2020-03-01T23:59:59'), true) //=> 1
  */
 
 import getDiffInCalendarYears from './getDiffInCalendarYears.js';
@@ -20,7 +15,7 @@ import getDiffInCalendarYears from './getDiffInCalendarYears.js';
 const getDiffInCalendarMonths = (
   targetDate: Date,
   baseDate: Date,
-  abs = true
+  abs = false
 ) => {
   const years = getDiffInCalendarYears(targetDate, baseDate, false);
 

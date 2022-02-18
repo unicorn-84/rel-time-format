4ef2da34ac63dea1383474b0421418a25afdb214
unicorn-84 <unicorn-84@ya.ru>
2022-02-18 16:27:55 +0300

    refactor(lib): change getRelQuarters

--- src/getRelQuarters.ts
+++ src/getRelQuarters.ts
@@ -1,24 +1,15 @@
+import getDiffInCalendarQuarters from './getDiffInCalendarQuarters.js';
+
 /**
  * Formats the date according to the [locales](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl#locale_identification_and_negotiation), formatting [options](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat#parameters) and the 'quarters' [unit](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/format#parameters) in human-friendly words relative to the current date.
  *
- *
  * @param {Date} date The date to format.
- *
  * @param {(string|string[])} [locales] A string with a [BCP 47 language tag](https://datatracker.ietf.org/doc/html/rfc4647#section-3.4), or an array of such strings.
- *
  * @param {Object} [options] An [object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat#Parameters) with some or all of options of `Intl.RelativeTimeFormatOptions`.
- *
- * @throws `TypeError` if `date` was given something other than the Date object.
- *
  * @returns {string} Internationalized relative time message as string.
- *
  * @example
- *
- * getRelQuarters(new Date(), 'en', { numeric: 'auto' })
- * //=> this quarter
+ * getRelQuarters(new Date(), 'en', { numeric: 'auto' }) //=> this quarter
  */
-
-import getDiffInCalendarQuarters from './getDiffInCalendarQuarters';
 
 const getRelQuarters = (
   date: Date,
@@ -27,13 +18,9 @@ const getRelQuarters = (
     | Intl.UnicodeBCP47LocaleIdentifier[],
   options?: Intl.RelativeTimeFormatOptions
 ): string => {
-  if (!(date instanceof Date)) {
-    throw new TypeError(`[rel-time-format]: '${date}' is not the Date object`);
-  }
-
   const formatter = new Intl.RelativeTimeFormat(locales, options);
 
-  const quarters = getDiffInCalendarQuarters(date, new Date(), false);
+  const quarters = getDiffInCalendarQuarters(date, new Date());
 
   return formatter.format(quarters, 'quarters');
 };

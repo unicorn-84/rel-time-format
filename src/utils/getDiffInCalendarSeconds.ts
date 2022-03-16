658296a75eea0c8413e36d99a358cebffa7ca991
unicorn-84 <unicorn-84@ya.ru>
2022-03-16 20:03:52 +0300

    chore(utils): change docs for getDiffInCalendarSeconds

--- src/utils/getDiffInCalendarSeconds.ts
+++ src/utils/getDiffInCalendarSeconds.ts
@@ -7,7 +7,6 @@
  * @returns The number of calendar seconds.
  * @example
  * getDiffInCalendarSeconds(new Date('2021-01-01T23:59:59.100'), new Date('2021-01-02T00:00:59.100')) //=> -60
- * getDiffInCalendarSeconds(new Date('2021-12-31T23:59:59.200'), new Date('2022-01-01T00:00:59.100'), true) //=> 60
  */
 
 const getDiffInCalendarSeconds = (

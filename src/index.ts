cb69507520feae442481c8ca5a54f0225d6e7441
unicorn-84 <unicorn-84@ya.ru>
2022-03-18 09:50:26 +0300

    fix: add export getDiffInMilliseconds

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,5 @@ export { default as getDiffInCalendarDays } from './getDiffInCalendarDays.js';
 export { default as getDiffInCalendarHours } from './getDiffInCalendarHours.js';
 export { default as getDiffInCalendarMinutes } from './getDiffInCalendarMinutes.js';
 export { default as getDiffInCalendarSeconds } from './getDiffInCalendarSeconds.js';
+
+export { default as getDiffInMilliseconds } from './getDiffInMilliseconds.js';
